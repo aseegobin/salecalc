a91b99f882efdb2cfee195e29ba2610e0023d9d4
Avie Seegobin <avieseegobin@gmail.com>
2016-06-02 00:52:23 -0400

    SaleCalc storyboard setup

---
+++ SaleCalc/SaleCalc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SaleCalc
+//
+//  Created by Avie Seegobin on 2016-06-02.
+//  Copyright © 2016 Avie Seegobin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
